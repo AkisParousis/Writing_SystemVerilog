353ab91127886f09d31ca32d2817d8b0f97a84c3
AkisParousis <33992774+AkisParousis@users.noreply.github.com>
2021-12-28 03:51:17 +0200

    Update testbench.sv

--- and8bit_complete_testbench/testbench.sv
+++ and8bit_complete_testbench/testbench.sv
@@ -101,3 +101,62 @@ class scoreboard;
     end
   endtask
 endclass
+
+class environment;
+  generator gen;
+  driver drv;
+  monitor mon;
+  scoreboard sco;
+  
+  virtual andt_intf vif;
+  
+  mailbox gendrvmbx;
+  mailbox monscombx;
+  
+  event gendrvdone;
+  
+  function new(mailbox gendrvmbx,mailbox monscombx);
+    this.gendrvmbx = gendrvmbx;
+    this.monscombx = monscombx;
+    
+    gen = new(gendrvmbx);
+    drv = new(gendrvmbx);
+    
+    mon = new(monscombx);
+    sco = new(monscombx);
+  endfunction
+  
+  task run();
+    gen.done = gendrvdone;
+    drv.done = gendrvdone;
+    
+    drv.vif = vif;
+    mon.vif = vif;
+    
+    fork
+      gen.run();
+      drv.run();
+      mon.run();
+      sco.run();
+    join_any
+  endtask
+endclass
+
+module tb();
+  environment env;
+  mailbox gendrvmbx;
+  mailbox monscombx;
+  
+  andt_intf vif();
+  andt_intf dut(vif.a,vif.b,vif,y);
+  
+  initial begin
+    gendrvmbx = new();
+    monscombx = new();
+    env = new(gendrvmbx,monscombx);
+    env.vif = vif;
+    env.run();
+    #200;
+    $finish;
+  end
+endmodule
